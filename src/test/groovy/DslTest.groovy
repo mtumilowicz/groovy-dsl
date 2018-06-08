ac15b2b998b6837ffe233e3b5dd86b1cac82ab92
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-06-08 23:40:22 +0200

    tests update

--- src/test/groovy/DslTest.groovy
+++ src/test/groovy/DslTest.groovy
@@ -1,6 +1,7 @@
+import org.xmlunit.builder.DiffBuilder
+import org.xmlunit.builder.Input
+import org.xmlunit.diff.Diff
 import spock.lang.Specification
-
-
 /**
  * Created by mtumilowicz on 2018-06-07.
  */
@@ -13,45 +14,62 @@ class DslTest extends Specification {
     
     def "xml"() {
         given:
-        DeadlineMemo.make {
+        String deadlineMemo = DeadlineMemo.make {
             title titleText
             deadline deadlineText
             idea ideaText
             plan planText
             xml
         }
-    }
-
-    def "html"() {
-        given:
-        DeadlineMemo.make {
-            title titleText
-            deadline deadlineText
-            idea ideaText
-            plan planText
-            html
-        }
+        
+        when:
+        Diff d = DiffBuilder.compare(Input.fromFile(new File("src/test/resources/DslTest.xml")))
+                .withTest(Input.fromString(deadlineMemo))
+                .ignoreWhitespace()
+                .ignoreComments()
+                .normalizeWhitespace()
+                .build()
+        
+        then:
+        !d.hasDifferences()
     }
 
     def "text"() {
         given:
-        DeadlineMemo.make {
+        String deadlineMemo = DeadlineMemo.make {
             title titleText
             deadline deadlineText
             idea ideaText
             plan planText
             text
         }
+        
+        and:
+        String control =
+                "Memo\n" +
+                        "Title: IMPORTANT\n" +
+                        "Deadline: 2020-01-01\n" +
+                        "idea: Be a better programmer!\n" +
+                        "plan: Commit to github everyday!\n"
+        
+        expect:
+        control == deadlineMemo
     }
 
     def "json"() {
         given:
-        DeadlineMemo.make {
+        String deadlineMemo = DeadlineMemo.make {
             title titleText
             deadline deadlineText
             idea ideaText
             plan planText
             json
         }
+        
+        and:
+        String control = '{"memo":{"title":"IMPORTANT","deadline":"2020-01-01","idea":"Be a better programmer!","plan":"Commit to github everyday!"}}'
+
+        expect:
+        deadlineMemo == control
     }
 }
